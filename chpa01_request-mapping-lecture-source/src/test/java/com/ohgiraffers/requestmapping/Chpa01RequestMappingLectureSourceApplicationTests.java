759b290f47417eba714870c54583602b8dc34e94
JungJay <keunhee0920@gmail.com>
2024-11-12 12:59:09 +0900

    [24.11.12] 스프링 부트 강의 추가

---
+++ chpa01_request-mapping-lecture-source/src/test/java/com/ohgiraffers/requestmapping/Chpa01RequestMappingLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.requestmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chpa01RequestMappingLectureSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
