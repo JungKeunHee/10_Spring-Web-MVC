828d6eebb5b2352da34466d7f9a8784a01e0325e
JungJay <keunhee0920@gmail.com>
2024-11-21 13:11:19 +0900

    [24.11.21] 스프링 crud 강의

---
+++ chap08-crud-lecture-source/src/main/java/com/ohgiraffers/crud/menu/model/dto/CategoryDTO.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.crud.menu.model.dto;
+
+import lombok.*;
+import org.apache.ibatis.type.Alias;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CategoryDTO {
+    private int code;
+    private String name;
+    private Integer refCategoryCode;
+}
